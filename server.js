3d47489899cb3573eee9ba40158c7c06dbc041e5
agomez99 <austine.gomez@yahoo.com>
2020-05-28 19:14:16 -0500

    server file connection

--- server.js
+++ server.js
@@ -2,23 +2,34 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config();
 
-const PORT = 3000;
+
+
+// For heroku Deployment
+var MONGODB_URI = process.env.MONGODB_URI || process.env.REMOTE;
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+//local connection
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true
+// });
 
 // routes
 app.use(require("./routes/api.js"));
